516a9038133b9410226f24577670f25efaf7c943
d-VorNick <70892582+d-VorNick@users.noreply.github.com>
2021-09-12 21:01:34 +0300

    Update lab2.sh

--- lab2.sh
+++ lab2.sh
@@ -24,4 +24,25 @@ ln -s /mnt/newdisk $HOME/newdisk_slink
 mkdir $HOME/newdisk_slink/catalog
 
 #9
-/dev/sda3 /mnt/newdisk ext4 noexec,noatime 0 0
+sudo nano /etc/fstab
+UUID=24c1ccbf-7de2-4573-abbf-edccfd3a7923 /mnt/newdisk ext4 noexec,noatime 0 0
+sudo reboot
+
+#10
+umount /dev/sda3
+reboot
+resize2fs /dev/sda3
+df -h
+
+#11
+fsck -n /dev/sda3
+
+#12
+mkfs.ext4 /dev/sda4
+umount /dev/sda3
+mke2fs -O journal_dev /dev/sda4
+mke2fs -O ^has_journal /dev/sda3
+mke2fs -t ext4 -J device=/dev/sda4 /dev/sda3
+##НЕ ЗАБЫТЬ ИЗМЕНИТЬ UUID В /etc/fstab
+reboot
+##ДЛЯ ПРОВЕРКИ tune2fs -l /dev/sda3 | grep features
