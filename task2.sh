f46e960a89f78f3c10ccf05d1238607a760ba84d
d-VorNick <70892582+d-VorNick@users.noreply.github.com>
2021-09-20 11:43:31 +0300

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,5 +1,14 @@
+#!/bin/sh
 
 echo "u4:x:1021:1021::/home/u4:/bin/bash" >> /etc/passwd
 echo "u4:x:1021:" >> /etc/group
 mkdir /home/u4
 cp /etc/skel/readme.txt /home/u4
+#получаем пароль 
+perl -e 'print crypt($ARGV[0], "password")' 123
+echo "u4:paN8aiEIonqJE:18884:0:99999:7:::" >> /etc/shadow
+
+chown -R u4 .
+chmod -R go=u,go-w
+chmod -R go=u,go-w .
+chmod go= .
